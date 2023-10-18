8bfe4c56f2c585eaeca617671324b106f4c13eac
zakiakhrn <zakiakhairunnisa97@gmail.com>
2023-10-18 22:03:34 +0700

    feat: soal 4

---
+++ Zakia_Khairunnisa-soal4-kelompok5.mjs
@@ -0,0 +1,7 @@
+function hitungluassegitiga(alas,tinggi) {
+    return (alas * tinggi) /2;
+}
+
+const luassegitiga = hitungluassegitiga(4, 5);
+
+console.log(`Luas segitiga dengan alas 4 dan tinggi 5 adalah: ${luassegitiga}`);
